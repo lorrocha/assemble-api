1be0914ec70f1239de694680e499c02488596612
Liz Baillie <liz@lizbaillie.com>
2016-12-01 09:02:39 -0800

    Try to get Google OAuth working :/ (w/ @locks)

--- config/application.rb
+++ config/application.rb
@@ -25,10 +25,13 @@ module AssembleApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-    config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'http://localhost:4200',
-    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-  }
+    config.api_only = false
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
