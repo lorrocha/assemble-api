ad0b21c7f264b1dd98e75c3c4f24e7525cbf041c
Liz Baillie <liz@lizbaillie.com>
2017-01-01 14:47:47 -0800

    Add users#me (current_user) to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'sessions' }
+  devise_for :users, controllers: { sessions: 'sessions' }, defaults: { format: :json }
+  get 'users/me' => 'users#me', as: 'current_user'
   resources :alerts
   resources :users do
     resources :teams
