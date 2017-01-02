de5a3e2dc930ae3daba4323bcc7b54cb9ea9cae6
Liz Baillie <liz@lizbaillie.com>
2017-01-02 15:43:13 -0800

    Replace before_filter with before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
   # This is our new function that comes before Devise's one
-  before_filter :authenticate_user_from_token!
+  before_action :authenticate_user_from_token!
   # This is Devise's authentication
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
 
   private
 
