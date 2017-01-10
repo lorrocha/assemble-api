3c831801f8db9ff43bdc5028245827a7f8b7d31f
L. Baillie <liz@lizbaillie.com>
2017-01-10 08:36:55 -0800

    Add initializer to allow additional Devise params

---
+++ config/initializers/devise_permitted_parameters.rb
@@ -0,0 +1,22 @@
+module DevisePermittedParameters
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :configure_permitted_parameters
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:username, :profile_text, :email)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:username, :profile_text, :email)
+    end
+  end
+
+end
+
+DeviseController.send :include, DevisePermittedParameters
