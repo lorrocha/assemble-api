454f07183693be762b77b8a192a9b3a9840709e5
Tim Wade <hello@timjwade.com>
2017-01-27 10:30:46 -0800

    Sign in for integration tests
    These tests are failing because Devise is kicking the requester
    out. Signing in with a valid user fixes that.

--- test/controllers/alerts_controller_test.rb
+++ test/controllers/alerts_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AlertsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    sign_in users(:alice)
     @alert = alerts(:one)
   end
 
