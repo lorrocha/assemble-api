4827f5a8986be50d1ec645744bea8d8ed87eda82
Tim Wade <hello@timjwade.com>
2017-02-04 21:00:28 -0800

    Remove dead users route and action
    There are two routes defined for the same pattern of `POST
    /users`. Devise's route is the one that is currently winning, so it
    should be safe to remove the other one and its corresponding action.
    
    Before:
    
    ```
    $ bundle exec rake routes | grep "POST   /users("
    POST   /users(.:format)    devise/registrations#create {:format=>:json}
    POST   /users(.:format)    users#create
    ```
    
    After:
    
    ```
    $ bundle exec rake routes | grep "POST   /users("
    POST   /users(.:format)    devise/registrations#create {:format=>:json}
    ```
    
    I also removed the route nested under teams, which is not technically
    dead but I don't believe it is being used.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'sessions' }, defaults: { format: :json }
   get 'users/me' => 'users#me', as: 'current_user'
   resources :alerts
-  resources :users do
+  resources :users, except: :create do
     resources :teams
   end
 
   resources :teams do
-    resources :users
+    resources :users, except: :create
     resources :alerts
   end
 end
