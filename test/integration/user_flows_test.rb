3bb7205c22307ceebc83dd9dcedff42972411fff
Tim Wade <hello@timjwade.com>
2017-02-06 14:01:45 -0800

    Incorporate user setup into test body

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class UserFlowsTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  setup do
-    @user = users(:alice)
-    sign_in @user
-  end
-
   test "should get index" do
+    sign_in users(:alice)
+
     get users_url, as: :json
+
     assert_response :success
   end
 
   test "should create user" do
+    sign_in users(:alice)
+
     assert_difference('User.count') do
       params = {
         user: {
@@ -29,18 +29,33 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
   end
 
   test "should show user" do
-    get user_url(@user), as: :json
+    user = users(:alice)
+    sign_in user
+
+    get user_url(user), as: :json
+
     assert_response :success
   end
 
+  # test "should not show user unless logged in" do
+
+  # end
+
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email } }, as: :json
+    user = users(:alice)
+    sign_in user
+
+    patch user_url(user), params: { user: { email: user.email } }, as: :json
+
     assert_response 200
   end
 
   test "should destroy user" do
+    user = users(:alice)
+    sign_in user
+
     assert_difference('User.count', -1) do
-      delete user_url(@user), as: :json
+      delete user_url(user), as: :json
     end
 
     assert_response 204
