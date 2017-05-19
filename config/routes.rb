efba4d81764212a25ca36e663c2c7f1e3c21d823
Tim Wade <hello@timjwade.com>
2017-05-19 09:06:56 -0700

    Give alerts shallow routes + index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'sessions' }, defaults: { format: :json }
   get 'users/me' => 'users#me', as: 'current_user'
-  resources :alerts
+
+  resources :alerts, only: :index
+
   resources :users, except: :create do
     resources :teams
   end
 
   resources :teams do
     resources :users, except: :create
-    resources :alerts
+    resources :alerts, shallow: true
   end
 end
